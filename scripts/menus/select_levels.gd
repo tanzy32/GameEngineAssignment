2470dceb150f3038a2b1e7a7ac6ffa4a6bb5ccf3
zyangTan <164876850+zyangTan@users.noreply.github.com>
2024-07-21 02:22:20 +0800

    added credits and level selection
    able to read at the credits and select level

---
+++ scripts/menus/select_levels.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+#	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_texture_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
+
+func _on_texture_button_2_pressed():
+	get_tree().change_scene_to_file("res://scenes/levels/level_2.tscn")
+
+func _on_texture_button_3_pressed():
+	get_tree().change_scene_to_file("res://scenes/levels/level_3.tscn")
+
+func _on_texture_button_4_pressed():
+	get_tree().change_scene_to_file("res://scenes/levels/level_4.tscn")
