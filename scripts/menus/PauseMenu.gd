dc66d27d64af59d9275a01e4c1f3f3da6036444a
zyangTan <164876850+zyangTan@users.noreply.github.com>
2024-07-23 04:44:11 +0800

    created pause menu and bug fixing
    solved some bug and created a pause menu

---
+++ scripts/menus/PauseMenu.gd
@@ -0,0 +1,33 @@
+extends Control
+
+@onready var canvas_layer = $CanvasLayer
+@onready var resume_texture_button = $CanvasLayer/PausePanel/CenterContainer/TextureRect/VBoxContainer/ResumeTextureButton
+
+func _ready():
+	canvas_layer.visible = false
+	get_tree().paused = false  # Ensure game is not paused on scene load
+
+func _input(event):
+	if event.is_action_pressed("pause"):
+		if canvas_layer.visible:
+			# Resume game
+			canvas_layer.visible = false
+			get_tree().paused = false
+		else:
+			# Show pause menu
+			canvas_layer.visible = true
+			get_tree().paused = true
+
+func _on_resume_texture_button_pressed():
+	canvas_layer.visible = false
+	get_tree().paused = false
+
+func _on_restart_texture_button_pressed():
+	get_tree().reload_current_scene()
+	
+
+func _on_settings_texture_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/menus/settings.tscn")
+
+func _on_main_menu_texture_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/menus/main_menu.tscn")
