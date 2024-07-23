df18399617fe2d99b9cf47a7f75e4ea8e5a3cb0a
zyangTan <164876850+zyangTan@users.noreply.github.com>
2024-07-23 23:49:39 +0800

    level 2 & 4

--- scripts/level_control.gd
+++ scripts/level_control.gd
@@ -4,10 +4,20 @@ var slime_scene: PackedScene = load("res://scenes/levels/level_1/slime.tscn")
 @onready var level_timer = $Player/countdown/level_timer
 @onready var spawner_timer = $Player/countdown/spawner_timer
 
+
+
 func _on_spawner_timer_timeout():
 	var slime = slime_scene.instantiate()
 	$Enemies.add_child(slime)
+	
+
 			
 func _process(delta):
 	if level_timer.is_stopped():
 		spawner_timer.stop()
+		
+		
+		
+		
+
+
