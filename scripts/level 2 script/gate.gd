ee0f91f2450667b11ba7e3eb4b0b317e58e8bbcf
cjlee020501@gmail.com <cjlee020501@gmail.com>
2024-09-14 10:26:24 +0800

    lvl 2 to boss room

--- scripts/level 2 script/gate.gd
+++ scripts/level 2 script/gate.gd
@@ -6,7 +6,7 @@ func _ready():
 	# Assuming the door has a child Area2D for detecting when the player is near
 	$Area2D.connect("body_entered", Callable(self, "_on_area_2d_body_entered"))
 
-func _on_button_is_button_active():
+func _on_button_is_active():
 	$AnimationPlayer.play("open_door")
 	is_door_open = true  # Mark the door as open
 	
@@ -14,4 +14,7 @@ func _on_button_is_button_active():
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("player") and is_door_open:
 		# Replace "res://path_to_new_scene.tscn" with the actual path to the new scene
-		get_tree().change_scene_to_file("res://scenes/levels/level 2/lvl2boss.tscn")
+		get_tree().change_scene_to_file("res://scenes/levels/level_2/lvl2boss.tscn")
+
+
+
