5a588e9fe0c63cd30b9be79521f6ab625678a2ae
zyangTan <164876850+zyangTan@users.noreply.github.com>
2024-09-14 09:42:42 +0800

    update

--- scripts/level_4 script/level_4_boss/LongRangeSkill.gd
+++ scripts/level_4 script/level_4_boss/LongRangeSkill.gd
@@ -8,15 +8,12 @@ func enter():
 	await play_animation("LongRangeSkill")
 	can_transition = true
 
+
 func play_animation(anim_name):
 	animation_player.play(anim_name)
 	$"../../Skill".play()
 	await animation_player.animation_finished
 
-#func set_target():
-	## Calculate the direction to flip based on owner's direction
-	#var flip = (owner.direction - pivot.position).x < 0 ? -1 : 1
-	#pivot.scale.x = flip
 	
 func transition():
 	if can_transition:
