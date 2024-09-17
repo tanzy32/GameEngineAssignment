f54d7b2b44b6c1d65f59ae85851e82d9557f0b7d
zyangTan <164876850+zyangTan@users.noreply.github.com>
2024-09-17 21:28:37 +0800

    update

--- scripts/level_1/base_scene.gd
+++ scripts/level_1/base_scene.gd
@@ -7,23 +7,5 @@ class_name BaseScene extends Node2D
 var has_last_scene: bool = false
 
 func _ready():
-	if SceneManagerLoad.player:
-		if player:
-			player.queue_free()
-			
-		player = SceneManagerLoad.player
-		add_child(player)
-		
-	position_player()
-	
-func position_player()->void:
-	var last_scene = SceneManagerLoad.last_scene_name
-	
-	if last_scene.is_empty():
-		has_last_scene = false
-	else:
-		has_last_scene = true
-		
-	for entrance in entrance_markers.get_children():
-		if entrance is Marker2D and last_scene:
-			player.global_position = entrance.global_position
+	pass
+
